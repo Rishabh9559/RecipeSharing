69b59ad887bcc40090e2ef45c175da5bc016b0e9
Rishabh Kushwaha <Rishabhkushwaha9559@gmail.com>
2024-10-27 16:25:16 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import {createBrowserRouter} from 'react-rute-dom'
 import "./App.css";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
+
+
 function App() {
 
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+
+const Footer = () => {
+  return (
+    <>
+    
+    
+    </>
+  )
+}
+
+export default Footer
